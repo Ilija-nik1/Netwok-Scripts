c480f84e7ff3d4a1e868a5a8e582f7138ab197fc
Ilija-nik1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-02-13 14:02:25 +0100

    Add files via upload

--- Network Scripts/IP address generator.js
+++ Network Scripts/IP address generator.js
@@ -1,8 +1,19 @@
 function generateRandomIP() {
-    return (Math.floor(Math.random() * 256) + 1) + "." +
-        (Math.floor(Math.random() * 256)) + "." +
-        (Math.floor(Math.random() * 256)) + "." +
-        (Math.floor(Math.random() * 256));
+    let ip = "";
+    for (let i = 0; i < 4; i++) {
+        let segment = Math.floor(Math.random() * 256);
+        // if segment is 0 or 255, try again
+        if (segment == 0 || segment == 255) {
+            i--;
+            continue;
+        }
+        ip += segment.toString();
+        // append a period if not last segment
+        if (i != 3) {
+            ip += ".";
+        }
+    }
+    return ip;
 }
 
 var ip = generateRandomIP();
