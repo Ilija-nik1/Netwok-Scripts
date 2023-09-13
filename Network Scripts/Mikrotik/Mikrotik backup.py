52f9fbc4c53333fcc9d2fce5d7c278561e71b19d
Ilija_Nik_1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-09-13 18:12:45 +0200

    Add files via upload

--- Network Scripts/Mikrotik/Mikrotik backup.py
+++ Network Scripts/Mikrotik/Mikrotik backup.py
@@ -1,52 +1,57 @@
 import os
 import paramiko
 import concurrent.futures
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler("router_config_download.log"), logging.StreamHandler()],
+)
 
 # Function to log in to a MikroTik router, export config, and download it
 def process_router(ip, username, password, output_dir):
     try:
         # Establish an SSH connection
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip, username=username, password=password)
+        with paramiko.SSHClient() as ssh:
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(ip, username=username, password=password)
 
-        # Run the export command
-        ssh.exec_command("/export file=config")
+            # Run the export command
+            ssh.exec_command("/export file=config")
 
-        # Wait for a moment to ensure the export is complete
-        import time
-        time.sleep(5)
+            # Wait for a moment to ensure the export is complete
+            import time
 
-        # Download the config file
-        scp = ssh.open_sftp()
-        remote_filename = "/config.rsc"
-        local_filename = os.path.join(output_dir, f"{ip}.rsc")
-        scp.get(remote_filename, local_filename)
-        scp.close()
+            time.sleep(5)
 
-        # Close the SSH connection
-        ssh.close()
-        print(f"Successfully downloaded config for {ip}")
+            # Download the config file
+            with ssh.open_sftp() as scp:
+                remote_filename = "/config.rsc"
+                local_filename = os.path.join(output_dir, f"{ip}.rsc")
+                scp.get(remote_filename, local_filename)
+
+            logging.info(f"Successfully downloaded config for {ip}")
     except Exception as e:
-        print(f"Error processing {ip}: {str(e)}")
+        logging.error(f"Error processing {ip}: {str(e)}")
 
-# Read Winbox addresses from the 'addresses.cib' file
-with open('addresses.cib', 'r') as file:
-    lines = file.readlines()
+if __name__ == "__main__":
+    # Read Winbox addresses from the 'addresses.cib' file
+    with open("addresses.cib", "r") as file:
+        addresses = [line.strip() for line in file.readlines()]
 
-addresses = [line.strip() for line in lines]
+    # Set your SSH username and password
+    username = "your_username"
+    password = "your_password"
 
-# Set your SSH username and password
-username = 'your_username'
-password = 'your_password'
+    # Set the directory where you want to save the config files
+    output_directory = "downloads"
 
-# Set the directory where you want to save the config files
-output_directory = 'downloads'
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_directory, exist_ok=True)
 
-# Create the output directory if it doesn't exist
-os.makedirs(output_directory, exist_ok=True)
-
-# Use concurrent.futures for multithreading
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    for address in addresses:
-        executor.submit(process_router, address, username, password, output_directory)
+    # Use concurrent.futures for multithreading
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for address in addresses:
+            executor.submit(process_router, address, username, password, output_directory)
