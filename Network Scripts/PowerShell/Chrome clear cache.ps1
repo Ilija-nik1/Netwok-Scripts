9d0cb1d633f15a53697d19a949052c888a27e860
Ilija_Nik_1 <110109065+Ilija-nik1@users.noreply.github.com>
2023-09-27 12:24:30 +0200

    Add files via upload

--- Network Scripts/PowerShell/Chrome clear cache.ps1
+++ Network Scripts/PowerShell/Chrome clear cache.ps1
@@ -2,22 +2,28 @@ $chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $numThreads = 2
 
 function Clear-Chrome-Cache {
+    param (
+        [string]$userDataDir,
+        [string]$chromePath
+    )
     try {
-        Stop-Process -Name "chrome" -Force
+        # Stop Chrome processes
+        Stop-Process -Name "chrome" -Force -ErrorAction SilentlyContinue
 
         $cacheClearCommand = @(
             $chromePath,
-            "--user-data-dir=TempProfile",
+            "--user-data-dir=$userDataDir",
             "--disk-cache-size=1",
             "--media-cache-size=1",
             "--disable-application-cache"
         )
 
+        # Start Chrome to clear cache
         Start-Process -FilePath $cacheClearCommand[0] -ArgumentList $cacheClearCommand[1..$cacheClearCommand.Length] -Wait
 
-        $tempProfileDir = Join-Path $env:USERPROFILE "TempProfile"
-        if (Test-Path $tempProfileDir) {
-            Remove-Item $tempProfileDir -Force -Recurse
+        # Remove the temporary profile directory
+        if (Test-Path $userDataDir) {
+            Remove-Item $userDataDir -Force -Recurse
         }
 
         Write-Host "Chrome cache has been cleared."
@@ -27,12 +33,32 @@ function Clear-Chrome-Cache {
     }
 }
 
-$threads = @()
+function Start-Multiple-Threads {
+    param (
+        [int]$numThreads,
+        [scriptblock]$scriptBlock
+    )
+    $threads = @()
 
-for ($i = 0; $i -lt $numThreads; $i++) {
-    $thread = [System.Threading.Thread]::new({ Clear-Chrome-Cache })
-    $threads += $thread
-    $thread.Start()
+    for ($i = 0; $i -lt $numThreads; $i++) {
+        $thread = [System.Threading.Thread]::new($scriptBlock)
+        $threads += $thread
+        $thread.Start()
+    }
+
+    $threads | ForEach-Object { $_.Join() }
 }
 
-$threads | ForEach-Object { $_.Join() }
+# Define a unique user data directory for each thread
+for ($i = 0; $i -lt $numThreads; $i++) {
+    $userDataDir = Join-Path $env:USERPROFILE "TempProfile$i"
+    
+    # Use the Start-Multiple-Threads function to run multiple threads
+    Start-Multiple-Threads -numThreads 2 -scriptBlock {
+        param (
+            [string]$userDataDir,
+            [string]$chromePath
+        )
+        Clear-Chrome-Cache -userDataDir $userDataDir -chromePath $chromePath
+    } -userDataDir $userDataDir -chromePath $chromePath
+}
--- Network Scripts/PowerShell/Connect to VPN.ps1
+++ Network Scripts/PowerShell/Connect to VPN.ps1
@@ -1,39 +1,55 @@
-# Get a list of available VPN connections
-$vpnConnections = Get-VpnConnection | Select-Object -Property Name, ServerAddress
-
-# Display the list of VPN connections
-Write-Host "Available VPN Connections:"
-$vpnConnections | ForEach-Object {
-    Write-Host "$($_.Name). $($_.ServerAddress)"
+function Get-VPNConnections {
+    # Get a list of available VPN connections
+    return Get-VpnConnection | Select-Object -Property Name, ServerAddress
 }
 
-# Prompt the user to select a VPN connection
-$selectedIndex = Read-Host "Enter the number of the VPN connection you want to connect to"
-
-# Validate the user's input
-if (-not [int]::TryParse($selectedIndex, [ref]$null) -or $selectedIndex -lt 1 -or $selectedIndex -gt $vpnConnections.Count) {
-    Write-Host "Invalid selection. Please enter a valid number."
-    exit 1
-}
-
-# Connect to the selected VPN connection
-$selectedConnection = $vpnConnections[$selectedIndex - 1].Name
-Write-Host "Connecting to $selectedConnection..."
-$connectResult = Connect-VpnS2SInterface -Name $selectedConnection -AsJob
-
-# Wait for the connection to complete
-$timeout = 60  # Adjust the timeout as needed
-try {
-    Wait-Job $connectResult -Timeout $timeout | Out-Null
-    Receive-Job $connectResult | ForEach-Object {
-        if ($_ -match "Connected") {
-            Write-Host "Connected to $selectedConnection."
-        } else {
-            Write-Host "Connection to $selectedConnection failed."
-        }
+function Display-VPNConnections($connections) {
+    # Display the list of VPN connections with numbers and spacing
+    Write-Host "Available VPN Connections:"
+    $connections | ForEach-Object {
+        $index = [array]::IndexOf($connections, $_) + 1
+        $formattedIndex = "{0,-3}" -f $index  # Add spacing for alignment
+        Write-Host "$formattedIndex. $($_.Name) - $($_.ServerAddress)"
     }
-} catch {
-    Write-Host "Connection to $selectedConnection failed: $_"
-} finally {
-    Remove-Job $connectResult
 }
+
+function Connect-To-VPN($connection) {
+    # Connect to the selected VPN connection (if available)
+    if ($connection) {
+        Write-Host "Connecting to $($connection.Name)..."
+        $connectResult = Connect-VpnS2SInterface -Name $connection.Name -AsJob
+
+        # Wait for the connection to complete
+        $timeout = 60  # Adjust the timeout as needed
+        try {
+            Wait-Job $connectResult -Timeout $timeout | Out-Null
+            $jobOutput = Receive-Job $connectResult -ErrorAction Stop
+            if ($jobOutput -match "Connected") {
+                Write-Host "Connected to $($connection.Name)."
+            } else {
+                Write-Host "Connection to $($connection.Name) failed."
+            }
+        } catch {
+            Write-Host "Connection to $($connection.Name) failed: $_"
+        } finally {
+            Remove-Job $connectResult
+        }
+    } else {
+        Write-Host "Selected VPN connection not found."
+    }
+}
+
+# Main script logic
+$vpnConnections = Get-VPNConnections
+Display-VPNConnections $vpnConnections
+
+do {
+    $selectedIndex = Read-Host "Enter the number of the VPN connection you want to connect to"
+
+    if (-not [int]::TryParse($selectedIndex, [ref]$null) -or $selectedIndex -lt 1 -or $selectedIndex -gt $vpnConnections.Count) {
+        Write-Host "Invalid selection. Please enter a valid number."
+    }
+} while (-not [int]::TryParse($selectedIndex, [ref]$null) -or $selectedIndex -lt 1 -or $selectedIndex -gt $vpnConnections.Count)
+
+$selectedConnection = $vpnConnections[$selectedIndex - 1]
+Connect-To-VPN $selectedConnection
